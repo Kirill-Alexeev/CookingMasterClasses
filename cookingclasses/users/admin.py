4ff054aecb8a7f0c9f51b9a081ea508abc5d21da
Kirill-Alexeev <kirushkalexeev@gmail.com>
2025-02-20 00:24:54 +0300

    made users admin

--- cookingclasses/users/admin.py
+++ cookingclasses/users/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "image")
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email", "image")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2", "image"),
+            },
+        ),
+    )
+
+    search_fields = ("username", "email", "first_name", "last_name")
+
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+
+    list_display_links = ("username", "email")
+
+    ordering = ("username",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
