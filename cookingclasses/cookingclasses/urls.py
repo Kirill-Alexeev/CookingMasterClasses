7c01a07169a41b53fe7947e6ef0791edb4f3b43d
Kirill-Alexeev <kirushkalexeev@gmail.com>
2025-02-26 22:26:37 +0300

    combined react and django

--- cookingclasses/cookingclasses/urls.py
+++ cookingclasses/cookingclasses/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls")),
     path("api/workshops/", include("workshops.urls")),
+    path("", TemplateView.as_view(template_name="index.html")),
 ]
 
 if settings.DEBUG:
