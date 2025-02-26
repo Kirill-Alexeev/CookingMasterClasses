888191e39158317f8ef966f68943738a42159bb3
Kirill-Alexeev <kirushkalexeev@gmail.com>
2025-02-26 21:24:23 +0300

    added api routes for workshops

--- cookingclasses/cookingclasses/urls.py
+++ cookingclasses/cookingclasses/urls.py
@@ -5,10 +5,9 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('workshops/', include('workshops.urls')),
-    path("", RedirectView.as_view(url="/workshops/", permanent=True)),
+    path("admin/", admin.site.urls),
+    path("api/users/", include("users.urls")),
+    path("api/workshops/", include("workshops.urls")),
 ]
 
 if settings.DEBUG:
--- cookingclasses/workshops/urls.py
+++ cookingclasses/workshops/urls.py
@@ -1,4 +1,28 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    CuisineViewSet,
+    RestaurantViewSet,
+    ChefViewSet,
+    RestaurantImageViewSet,
+    MasterClassViewSet,
+    RecordViewSet,
+    ReviewViewSet,
+    VideoViewSet,
+    LikeViewSet,
+)
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r"cuisines", CuisineViewSet)
+router.register(r"restaurants", RestaurantViewSet)
+router.register(r"chefs", ChefViewSet)
+router.register(r"restaurant-images", RestaurantImageViewSet)
+router.register(r"master-classes", MasterClassViewSet)
+router.register(r"records", RecordViewSet)
+router.register(r"reviews", ReviewViewSet)
+router.register(r"videos", VideoViewSet)
+router.register(r"likes", LikeViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
