a5264472390d174ebdda93c8599bce7872a9b6b2
Kirill-Alexeev <kirushkalexeev@gmail.com>
2025-02-26 20:44:44 +0300

    added views for workshops

--- cookingclasses/workshops/views.py
+++ cookingclasses/workshops/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import (
+    Cuisine,
+    Restaurant,
+    Chef,
+    RestaurantImage,
+    MasterClass,
+    Record,
+    Review,
+    Video,
+    Like,
+)
+from .serializers import (
+    CuisineSerializer,
+    RestaurantSerializer,
+    ChefSerializer,
+    RestaurantImageSerializer,
+    MasterClassSerializer,
+    RecordSerializer,
+    ReviewSerializer,
+    VideoSerializer,
+    LikeSerializer,
+)
 
-# Create your views here.
+
+class CuisineViewSet(viewsets.ModelViewSet):
+    queryset = Cuisine.objects.all()
+    serializer_class = CuisineSerializer
+
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+
+class ChefViewSet(viewsets.ModelViewSet):
+    queryset = Chef.objects.all()
+    serializer_class = ChefSerializer
+
+
+class RestaurantImageViewSet(viewsets.ModelViewSet):
+    queryset = RestaurantImage.objects.all()
+    serializer_class = RestaurantImageSerializer
+
+
+class MasterClassViewSet(viewsets.ModelViewSet):
+    queryset = MasterClass.objects.all()
+    serializer_class = MasterClassSerializer
+
+
+class RecordViewSet(viewsets.ModelViewSet):
+    queryset = Record.objects.all()
+    serializer_class = RecordSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class VideoViewSet(viewsets.ModelViewSet):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
