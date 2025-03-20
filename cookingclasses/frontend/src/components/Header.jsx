0f63522f3caa40169427905cbe2deef0d6c987a9
Kirill-Alexeev <kirushkalexeev@gmail.com>
2025-03-20 15:45:00 +0300

    changed Header styles

--- cookingclasses/frontend/src/components/Header.jsx
+++ cookingclasses/frontend/src/components/Header.jsx
@@ -4,6 +4,8 @@ import { getUserProfile, logoutUser } from "../api/users";
 import kylinarLogo from "../assets/icons/kylinar_logo.svg";
 import mailIcon from "../assets/icons/mail_icon.svg";
 import phoneIcon from "../assets/icons/phone_icon.svg";
+import searchIcon from "../assets/icons/search_icon.svg";
+import locationIcon from "../assets/icons/location_icon.svg";
 
 function Header() {
   const [user, setUser] = useState(null);
@@ -27,79 +29,90 @@ function Header() {
 
   return (
     <header className="header">
-      <div className="header__top">
-        <div className="header__logo">
-          <img src={kylinarLogo} alt="Кулинар" className="header__logo-img" />
+      <div className="header__wrapper">
+        <div className="header__location">
+          <button className="header__location-btn">
+            <img src={locationIcon} className="header__location-img" />
+            Ваш адрес
+          </button>
         </div>
-        <div className="header__search">
-          <input
-            type="text"
-            placeholder="Поиск по сайту"
-            className="header__search-input"
-          />
+        <div className="header__top">
+          <NavLink to="/" className="header__logo" end>
+            <img src={kylinarLogo} alt="Кулинар" className="header__logo-img" />
+          </NavLink>
+          <div className="header__search">
+            <input
+              type="text"
+              placeholder="Поиск по сайту"
+              className="header__search-input"
+            />
+            <button className="header__search-btn">
+              <img src={searchIcon} className="header__search-img" />
+            </button>
+          </div>
+          <div className="header__contacts">
+            <a href="mailto:kylinar@mail.ru" className="header__contacts-link">
+              <img src={mailIcon} className="header__contactsimg" />
+              kylinar@mail.ru
+            </a>
+            <a href="tel:+79500000000" className="header__contacts-link">
+              <img src={phoneIcon} className="header__contacts-img" />
+              +7 (950) 000-00-00
+            </a>
+          </div>
         </div>
-        <div className="contacts">
-          <a href="mailto:kylinar@mail.ru" className="contacts__email">
-            <img src={mailIcon} className="contacts__img" />
-            kylinar@mail.ru
-          </a>
-          <a href="tel:+79500000000" className="contacts__phone">
-            <img src={phoneIcon} className="contacts__img" />
-            +7 (950) 000-00-00
-          </a>
-        </div>
+        <nav className="header__nav">
+          <ul className="header__nav-list">
+            <li className="header__nav-item">
+              <NavLink to="/" className="header__nav-link" end>
+                Главная
+              </NavLink>
+            </li>
+            <li className="header__nav-item">
+              <NavLink to="/restaurant-list" className="header__nav-link">
+                Рестораны
+              </NavLink>
+            </li>
+            <li className="header__nav-item">
+              <NavLink to="/master-class-list" className="header__nav-link">
+                Мастер-классы
+              </NavLink>
+            </li>
+            <li className="header__nav-item">
+              <NavLink to="/video-list" className="header__nav-link">
+                Видео
+              </NavLink>
+            </li>
+            {user ? (
+              <>
+                <li className="header__nav-item">
+                  <NavLink to="/profile" className="header__nav-link">
+                    Профиль
+                  </NavLink>
+                </li>
+                <li className="header__nav-item">
+                  <button className="header__nav-btn" onClick={handleLogout}>
+                    Выйти
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="header__nav-item">
+                  <NavLink to="/register" className="header__nav-link">
+                    Зарегистрироваться
+                  </NavLink>
+                </li>
+                <li className="header__nav-item">
+                  <NavLink to="/login" className="header__nav-link">
+                    Войти
+                  </NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
       </div>
-      <nav className="header__nav">
-        <ul className="header__nav-list">
-          <li className="header__nav-item">
-            <NavLink to="/" className="header__nav-link" end>
-              Главная
-            </NavLink>
-          </li>
-          <li className="header__nav-item">
-            <NavLink to="/restaurant-list" className="header__nav-link">
-              Рестораны
-            </NavLink>
-          </li>
-          <li className="header__nav-item">
-            <NavLink to="/master-class-list" className="header__nav-link">
-              Мастер-классы
-            </NavLink>
-          </li>
-          <li className="header__nav-item">
-            <NavLink to="/video-list" className="header__nav-link">
-              Видео
-            </NavLink>
-          </li>
-          {user ? (
-            <>
-              <li className="header__nav-item">
-                <NavLink to="/profile" className="header__nav-link">
-                  Профиль
-                </NavLink>
-              </li>
-              <li className="header__nav-item">
-                <button className="header__nav-button" onClick={handleLogout}>
-                  Выйти
-                </button>
-              </li>
-            </>
-          ) : (
-            <>
-              <li className="header__nav-item">
-                <NavLink to="/register" className="header__nav-link">
-                  Зарегистрироваться
-                </NavLink>
-              </li>
-              <li className="header__nav-item">
-                <NavLink to="/login" className="header__nav-link">
-                  Войти
-                </NavLink>
-              </li>
-            </>
-          )}
-        </ul>
-      </nav>
     </header>
   );
 }
